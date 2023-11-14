f730045557419c8b48c7f5d3bfa862604271ffa5
Loïc Kami wsl <lkami@bogital.com>
2023-11-14 11:47:58 +0100

    Add utils_num.c with helper functions: count_digits, print_number and _print_digit

---
+++ utils_num.c
@@ -0,0 +1,85 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * count_digits - Calculates the number of digits in a given number.
+ * @n: The number for which digits need to be counted.
+ *
+ * Return: The number of digits.
+ */
+int count_digits(unsigned long n)
+{
+	int count = 0;
+
+	if (n == 0)
+		return (1);
+
+	while (n > 0)
+	{
+		n /= 10;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * print_number - Prints a number and returns the number of characters printed.
+ * @n: The number to be printed.
+ *
+ * Return: The number of characters printed.
+ */
+int print_number(unsigned long n)
+{
+	int num_digits, *digits, i;
+
+	/* Handle special case when n is 0 */
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	num_digits = count_digits(n);
+	digits = malloc(num_digits * sizeof(int));
+
+	if (digits == NULL)
+	{
+		/* TODO: Should exit failure */
+		return (0);
+	}
+
+	/* Extract digits and store them in the array */
+	for (i = num_digits - 1; i >= 0; i--)
+	{
+		digits[i] = n % 10;
+		n /= 10;
+	}
+
+	for (i = 0; i < num_digits; i++)
+	{
+		_putchar(digits[i] + '0');
+	}
+
+	return (num_digits);
+}
+
+/**
+ * _print_digit - A function that prints a digit or number
+ * to the standard output stream
+ * @num: the number to print
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int _print_digit(int num)
+{
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	return (print_number((unsigned long)num));
+}
