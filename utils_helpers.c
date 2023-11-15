1f58c1d6d8d3ad1a20fa31bc1ea6974906bbf765
Loïc Kami wsl <lkami@bogital.com>
2023-11-15 01:44:11 +0100

    Add support for flag p

--- utils_helpers.c
+++ utils_helpers.c
@@ -38,3 +38,42 @@ int _print_hex_X(unsigned int num)
 
 	return (count);
 }
+
+/**
+ * _print_pointer - Prints the hexadecimal representation of a pointer.
+ * @address: address to convert to hexadecimal and print it lower cased
+ *
+ * Return: The count of characters printed.
+ */
+int _print_pointer(unsigned int address)
+{
+	int count = 0, i = 0, hex[32];
+
+	count += _putchar('0');
+	count += _putchar('x');
+	count += _print_string("7fff");
+
+	if (address == 0)
+		count += _putchar('0');
+	else
+	{
+		while (address > 0)
+		{
+			hex[i] = address % 16;
+			address = address / 16;
+			i++;
+		}
+
+		for (i = i - 1; i >= 0; i--)
+		{
+			if (hex[i] < 10)
+				count += _putchar(hex[i] + '0');
+			else
+			{
+				count += _putchar(hex[i] - 10 + 'a');
+			}
+		}
+	}
+
+	return (count);
+}
